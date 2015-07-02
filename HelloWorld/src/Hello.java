84335f6525d196f8c703ae4742421e329a83c8bc
pauljuneau <pjuneau@asu.edu>
2015-07-01 23:57:37 -0400

    test2

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+	/**
+	 * Implement the application
+	 * @param args arguments
+	 * 
+	 * Track changes by right clicking on the project folder --> Team --> Add to Index
+	 * Then Team --> Commit
+	 */
+	public static void main(String [] args) {
+		System.out.println("Hello Motha Fucka!!");
+	}
+}
